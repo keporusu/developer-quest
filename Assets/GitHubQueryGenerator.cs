cdb25842184338ade13d4cde295c85c55e9cb39b
tetsu <kepokeke1059@gmail.com>
2024-06-02 13:57:31 +0900

    なんとかContributionを取得しました

--- Assets/GitHubQueryGenerator.cs
+++ Assets/GitHubQueryGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
@@ -7,35 +8,67 @@ using UnityEngine.UI;
 using GraphQL.Client;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] private Button _queryButton;
-    [SerializeField] private Text _queryInputField;
+    [SerializeField] private Button _loginQueryButton;
+    [SerializeField] private Button _contributionQueryButton;
+    [SerializeField] private Text _loginQueryInputField;
+    [SerializeField] private Text _contributionQueryInputField;
+    [CanBeNull] private string _userName;
+    [CanBeNull] private string _from = "";
+    
     private string _apiKey = "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";
     void Start()
     {
-        _queryButton.onClick.AddListener(() =>
+        _loginQueryButton.onClick.AddListener(() =>
         {
-            _sendQuery();
+            _sendLoginQuery();
+        });
+        _contributionQueryButton.onClick.AddListener(() =>
+        {
+            _sendContributionsQuery();
         });
     }
 
-    private async void _sendQuery()
+    private async void _sendLoginQuery()
     {
         GraphQLHttpClient graphQLClient = new GraphQLHttpClient($"https://api.github.com/graphql", new NewtonsoftJsonSerializer());
         graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + _apiKey);
         //graphQLClient.Options.MediaType = "application/json";
-        
-        
 
         GraphQLRequest query = new GraphQLRequest
         {
-            Query = _queryInputField.text,
+            Query = _loginQueryInputField.text,
         };
 
         var response = await graphQLClient.SendQueryAsync<GitHubQueryResponse>(query, CancellationToken.None);
+        
+        Debug.Log($"[Query] {JsonConvert.SerializeObject(response.Data)}");
+
+        _userName = response.Data.Viewer.Login;
+    }
+    
+    private async void _sendContributionsQuery()
+    {
+        GraphQLHttpClient graphQLClient = new GraphQLHttpClient($"https://api.github.com/graphql", new NewtonsoftJsonSerializer());
+        graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + _apiKey);
+        
+        //graphQLClient.Options.MediaType = "application/json";
+        
+        var variables = new {
+            userName = _userName,
+        };
+
+        GraphQLRequest query = new GraphQLRequest
+        {
+            Query = _contributionQueryInputField.text,
+            Variables = variables
+        };
+
+        var response = await graphQLClient.SendQueryAsync<ContributionInfo>(query, CancellationToken.None);
 
         Debug.Log($"[Query] {JsonConvert.SerializeObject(response.Data)}");
     }
--- Assets/GitHubQueryResponse.cs
+++ Assets/GitHubQueryResponse.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//ログイン・テスト
 public class GitHubQueryResponse
 {
     public Viewer Viewer { get; set; }
@@ -10,4 +11,34 @@ public class GitHubQueryResponse
 public class Viewer
 {
     public string Login { get; set; }
+}
+
+
+
+//草取得
+public class ContributionInfo
+{
+    public User User { get; set; }
+}
+public class User
+{
+    public ContributionsCollection ContributionsCollection { get; set; }
+}
+public class ContributionsCollection
+{
+    public ContributionCalendar ContributionCalendar { get; set; }
+}
+public class ContributionCalendar
+{
+    public int TotalContributions { get; set; }
+    public Week[] Weeks { get; set; }
+}
+public class Week
+{
+    public ContributionDay[] ContributionDays { get; set; }
+}
+public class ContributionDay
+{
+    public int ContributionCount { get; set; }
+    public string Date { get; set; }
 }
