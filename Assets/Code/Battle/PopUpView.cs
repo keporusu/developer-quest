adeac1e1bcf92b1ff9ff7b351248fc40f911858e
keporusu <kepokeke1059@gmail.com>
2024-09-14 14:27:07 +0900

    いろいろ追加

--- Assets/Code/Battle/PopUpView.cs
+++ Assets/Code/Battle/PopUpView.cs
@@ -17,6 +17,9 @@ namespace Code.Battle
         [SerializeField] private GameObject _levelGage;
         [SerializeField] private Button _okButton;
 
+        [SerializeField] private int _maxExperience = 80000;
+        [SerializeField] private int _maxLevel = 120;
+
         private void _initialize()
         {
             _okButton.onClick.AddListener(_inActivate);
@@ -34,7 +37,48 @@ namespace Code.Battle
         {
             gameObject.transform.DOScale(Vector3.zero, 0.2f)
                 .OnComplete(()=>gameObject.SetActive(false));
+        }
+
+        public void SetHp(int hp)
+        {
+            _updateNum(hp, _hpNum);
+        }
+
+        public void SetCp(int cp)
+        {
+            _updateNum(cp,_contributionNum);
+        }
+
+        public void SetDamage(int damage)
+        {
+            _updateNum(damage, _damageNum);
+        }
+
+        public void SetExperience(int ex)
+        {
+            _updateGage(ex, _maxExperience,_experienceGage);
+        }
+        public void SetLevel(int level)
+        {
+            _updateGage(level,_maxLevel,_levelGage);
+        }
+
+        private void _updateGage(int res, int max, GameObject gage)
+        {
+            var scale = gage.transform.localScale;
             
+            gage.transform.DOScale(new Vector3((float)res / max, scale.y, scale.z), 1.0f)
+                .SetEase(Ease.OutQuad);
+            //else gage.transform.localScale = new Vector3((float)res / max,scale.y,scale.z);
+        }
+
+        private void _updateNum(int res, TextMeshProUGUI text)
+        {
+            DOTween.To(() => 0, x => 
+                {
+                    text.text = x.ToString();
+                }, res, 0.5f)
+                .SetEase(Ease.OutQuad);
         }
         
     }
